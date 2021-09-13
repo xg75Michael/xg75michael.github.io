e95b4c82e6df0236639166b2c1feaeafe86c6bfa
Michael <xg75@students.waikato.ac.nz>
2021-09-13 20:16:24 +0800

    update
    update

--- js/compass-clock.js
+++ js/compass-clock.js
@@ -1,17 +1,45 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function domloaded() {
         console.info('compass clock loading');
-        let _dial = document.querySelector('.dial-container');
-        if (!!_dial) {
-            let _seconds = _dial.querySelectorAll('.dial-second span');
-            let _minute = _dial.querySelectorAll('.dial-minute span');
-            let _hour = _dial.querySelectorAll('.dial-hour span');
-            let _s_d = 200;
-            let _s_r = 360 / _seconds.length;
-            _seconds.forEach(function (e, i) {
-                console.info(e);
-                e.style.transform = 'rotate(' + i * _s_r + 'deg) translateX(' + _s_d + 'px)';
-            });
+
+        const CIRCLER = 360;
+        let compassCt = (function setCompassDial(s, ss, sm, sh, sd) {
+            let _dial = document.querySelector(s);
+            let _seconds = _dial.querySelectorAll(ss);
+            let _minutes = _dial.querySelectorAll(sm);
+            let _hours = _dial.querySelectorAll(sh);
+            let _decorat = _dial.querySelectorAll(sd);
+            let _space = 50;
+            if (!!_dial) {
+                [_seconds, _minutes, _hours, _decorat].forEach(function (g, gi) {
+                    gi < 3 ?
+                        g.forEach(function (e, i) {
+                            e.style.transform = 'rotate(' + i * CIRCLER / g.length + 'deg) translateX(' + (200 - gi * _space) + 'px)';
+                        }) :
+                        g.forEach(function (e, i) {
+                            e.style.transform = 'rotate(' + i * CIRCLER / g.length + 'deg) translateX(' + 240 + 'px)';
+                        })
+                });
+            }
+            return {
+                container: _dial,
+                seconds: _seconds,
+                minutes: _minutes,
+                hours: _hours,
+            };
+        })('.dial-container', '.dial-second span', '.dial-minute span', '.dial-hour span', '.dial-decoration span');
+        console.info(compassCt);
+
+        function handleRaf(t) {
+            let nNow = new Date();
+            let sR = -CIRCLER / compassCt.seconds.length * nNow.getSeconds();
+            let mR = -CIRCLER / compassCt.minutes.length * nNow.getMinutes();
+            let hR = -CIRCLER / compassCt.hours.length * (nNow.getHours() - 1);
+            document.querySelector('.dial-second').style.transform = 'rotate(' + sR + 'deg)';
+            document.querySelector('.dial-minute').style.transform = 'rotate(' + mR + 'deg)';
+            document.querySelector('.dial-hour').style.transform = 'rotate(' + hR + 'deg)';
+            requestAnimationFrame(handleRaf);
         }
+        requestAnimationFrame(handleRaf);
     });
 })();
